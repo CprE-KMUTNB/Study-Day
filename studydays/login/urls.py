28f6fcb2af0e79739b1ca95bbe904611b83b3d31
Apiravit <s6401012610034@email.kmutnb.ac.th>
2022-10-08 21:02:16 +0700

    login 0.2

--- studydays/login/urls.py
+++ studydays/login/urls.py
@@ -4,6 +4,5 @@ from django.urls import path,include
 urlpatterns = [
     path('register',views.register.as_view()),
     path('login', views.LoginView.as_view()),
-    path('user', views.UserView.as_view()),
     path('logout', views.LogoutView.as_view())
 ]
--- studydays/login/views.py
+++ studydays/login/views.py
@@ -48,21 +48,6 @@ class LoginView(APIView):
         return response
 
 
-class UserView(APIView):
-    def get(self, request):
-        token = request.COOKIES.get('jwt')
-
-        if not token:
-            raise AuthenticationFailed('Unauthenticated!')
-        try:
-            payload = jwt.encode(token, 'secret', algorithm=['HS256'])
-        except jwt.ExpiredSignatureError:
-            raise AuthenticationFailed('Unauthenticated!')
-
-        user = User.objects.filter(id = payload['id']).first()
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-
 class LogoutView(APIView):
     def post(self, request):
         response = Response()
