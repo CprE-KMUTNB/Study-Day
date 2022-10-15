cd27b04fec342f166ef09678b2a21a9b7dcabbda
DogeDios <ggggezwpez@gmail.com>
2022-10-15 14:09:58 +0700

    Add calendar to main

--- src/Pages/main.js
+++ src/Pages/main.js
@@ -1,12 +1,57 @@
-import React from 'react'
+import React , {useState,useEffect} from 'react'
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
+import timeGridPlugin from '@fullcalendar/timegrid'
+import '../Styles/Main.css'
 
+const Main = () => {
+  const [show, setShow] = useState(false);
+  const [value,setValue] = useState({
+    tittle:'',
+    start:'',
+    end:''
+  })
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-function Main() {
-  
+  const handleSelect=(info)=>{
+    handleShow();
+    console.log(info)
+    setValue({
+      ...value,
+      start:info.startStr,
+      end:info.endStr
+    })
+  }
+  const onChangeValues=(e)=>{
+    console.log(e.target.value)
+    setValue({...value,[e.target.name]:e.target.value})
+  }
 
   return (
-    <h1>This is mainpage</h1>
+    <div className='componentmain'>
+      <div className='leftside'>
+        <input type='text place ' placeholder='Tittle' ></input>
+      </div>
+      <div className='rightside'>
+        <FullCalendar
+            plugins={[ dayGridPlugin, timeGridPlugin,interactionPlugin ]}
+            headerToolbar={{
+              center:'title',
+              left:'dayGridMonth,timeGridWeek,timeGridDay'
+            }}
+            selectable={true}
+            select={handleSelect}
+          />
+          
+
+          
+      </div>
+      
+    </div>
+
   )
 }
-
 export default Main
+
