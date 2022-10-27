7b07bd6f00cdc27295ed22192a80fa960a033eb9
DogeDios <ggggezwpez@gmail.com>
2022-10-27 21:39:30 +0700

    Connect Calendar

--- src/components/function/fullcalendar.js
+++ src/components/function/fullcalendar.js
@@ -1,15 +1,16 @@
 import { config } from "@fullcalendar/react";
 import axios from "axios";
 const idp=localStorage.getItem('eventid')
-
+const jwt = localStorage.getItem('token')
 export const createEvent = async(values)=>
-    await axios.post('http://127.0.0.1:8000/home/calender/',values); //API Path,values
+                                                                    //get header
+    await axios.post('http://127.0.0.1:8000/home/calender/',values,{headers:{authorization:`Bearer ${jwt}`}}); 
 
 export const listEvent = async()=>
-    await axios.get('http://127.0.0.1:8000/home/calender/') //API Path
+    await axios.get('http://127.0.0.1:8000/home/calender/',{headers:{authorization:`Bearer ${jwt}`}}); 
 
 export const updateEvent = async(values,idl)=>
-    await axios.put('http://127.0.0.1:8000/home/calender/'+idl+'/',values) //API Path
+    await axios.put('http://127.0.0.1:8000/home/calender/'+idl+'/',values,{headers:{authorization:`Bearer ${jwt}`}});  
 
 export const deleteEvent = async(values)=>
-    await axios.delete('http://127.0.0.1:8000/home/calender/'+values+'/') //API Path
+    await axios.delete('http://127.0.0.1:8000/home/calender/'+values+'/',{headers:{authorization:`Bearer ${jwt}`}});  
